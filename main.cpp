8f857e9f9fbc40752cc472d0bd743790ff5019f2
muratpala007 <muratpala007@gmail.com>
2022-04-19 13:36:11 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sayilar[] = {1,2,3,4,5,6};
+
+    for(int herbirsayi : sayilar){
+        cout<<herbirsayi;
+    }
+    return 0;
+
+}
